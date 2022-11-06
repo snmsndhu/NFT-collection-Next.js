e09786ba1d1b6f9281b96004076e4722033e398a
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2022-11-06 07:43:42 -0500

    update

--- constants/index.js
+++ constants/index.js
@@ -1,2 +1,2 @@
 export const abi =---your abi---
-export const NFT_CONTRACT_ADDRESS = "address of your NFT contract"
+export const NFT_CONTRACT_ADDRESS = "0x6b27985BfaFfB54b8fDFa7509035A8F7fD8139B9"
